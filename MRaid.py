436e111d4f28a9003530084b247609f10435e3ed
Samuel <58642126+Malicioums@users.noreply.github.com>
2019-12-13 19:00:19 +0100

    Fichier principal

---
+++ MRaid.py
@@ -0,0 +1,119 @@
+import time
+import discord
+from itertools import cycle
+from discord.ext import commands, tasks
+
+client = commands.Bot(command_prefix = "Mr/")
+status = cycle(["programme : MRaid", "Github : Malicioums"])
+
+titre = ("\f┌──────────────────────────────────┐\n\
+	....	     ....  ........\n\
+	.....       .....  ..........\n\
+	.......   .......  ...   ....\n\
+	.... ....... ....  ..........\n\
+	....  .....  ....  ....  ....\n\
+	....   ...   ....  ....   ....\n\
+	....    .    ....  ....    ....AID™\n\
+\f├──────────────────────────────────┤\n\
+\fquit  : Quitter MRaid\n\
+hraid : Afficher les commandes de raid\n\
+info  : Donne les liens de MRaid \n\
+menu  : Aller au menu principal (ici)\n\
+		\f	MRaid by Malicioums\n\
+\f└──────────────────────────────────┘\f")
+
+text_info = "\f┌──────────────────────────────────┐\n\
+\f	....	     ....  ........\n\
+	.....       .....  ..........\n\
+	.......   .......  ...   ....\n\
+	.... ....... ....  ..........\n\
+	....  .....  ....  ....  ....\n\
+	....   ...   ....  ....   ....\n\
+	....    .    ....  ....    ....AID™\n\
+\f	   créé part Malicioums\n\
+\f├──────────────────────────────────┤\n\
+\fLien du serveur Discord :\n\
+\fLien de la chaîne Youtube :\n\
+\f└──────────────────────────────────┘\f"
+
+info_hraid = "\f┌───────────────────────┐\n\
+│		    	     │\n\
+│      Commandes de raid     │\n\
+│                            │\n├───────────────────────┤\n\
+│		             │\n\
+│		             │\n\
+├	spammeur :           ┤\n\
+│Sert à paramétrer la        │\n\
+│commande spam.              │\n\
+│		             │\n\
+├	run :	             ┤\n\
+│Sert à entrer le token      │\n\
+│du bot.  	             │\n\
+│		             │\n\
+├		             ┤\n\
+│		             │\n\
+│		             │\n\
+└───────────────────────┘\f"
+
+def bot_disc():
+	@tasks.loop(seconds=20)
+	async def change_status():
+		await client.change_presence(activity=discord.Game(next(status)))
+		
+	@client.event
+	async def on_ready():
+		change_status.start()
+		print("\f┌──────────────────────────┐\n│	Bot connecter	         │\n│Nom du bot:", client.user.name,"         │" , "\n│ID du bot:", client.user.id, "  │", "\n└──────────────────────────┘\f")
+
+message_a = ""
+nombre_m = ""
+print(titre)
+launched = True
+nombre = 5
+cmd = input("[-MRaid-] >> ")
+token_test = ""
+while launched:
+	if cmd == "quit":
+		print(f"[-MRaid-] Fermeture dans {nombre} seconde")
+		time.sleep(1)
+		nombre -= 1
+		if nombre == 0:
+			print("[-MRaid-] Vous avez quitté MRaid...")
+			launched = False
+	elif cmd == "info":
+		print(text_info)
+		cmd = input("[-MRaid-] >> ")
+	elif cmd == "hraid":
+		print(info_hraid)
+		cmd = input("[-MRaid-] >> ")
+	elif cmd == "menu":
+		print(titre)
+		cmd = input("[-MRaid-] >> ")
+	elif cmd == "run":
+		token_test = input("[-MRaid-] Entrer votre token : ")
+		bot_disc()
+		client.run(token_test)
+		cmd = input("[-MRaid-] >> ")
+	elif cmd == "spammeur":
+		message_a = input("[-MRaid-] Entrer le message à spammer : ")
+		nombre_m = input("[-MRaid-] Entrer le nombre de message à spammer : ")
+		cmd = input("[-MRaid-] >> ")
+		@client.command()
+		async def spam(ctx):
+			go_spam = True
+			compteur = int(nombre_m)
+			while go_spam:
+				await ctx.send(f"> {message_a} \n> ``[{compteur}]``")
+				time.sleep(1)
+				compteur-= 1
+				if compteur == 0:
+					await ctx.send("```spam terminer...```")
+					go_spam = False
+	elif cmd == "spammeur--info":
+		print("\f┌──────────────────────────┐\n             spammeur	     \n \f contenu du message:\n", message_a, "\n \f nombre de message à spam:\n", nombre_m, "\n \n└─────────────────────────┘\f")
+		cmd = input("[-MRaid-] >> ")
+		pass
+	else:
+		print("[-MRaid-] Cette commande n'existe pas...")
+		cmd = input("[-MRaid-] >> ")
+		continue
